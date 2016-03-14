0db72b2cc8547a2711d0f894cbc00251d26476d3
Alexander Hansson <Alex@Alexanders-MacBook-Pro-2.local>
2016-03-14 11:28:39 +0100

    Structure for API complete with controllers and models and relations

---
+++ api/controllers/SessionsController.js
@@ -0,0 +1,17 @@
+/**
+ * SessionsController
+ *
+ * @description :: Server-side logic for managing sessions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	getSession: function(req, res) {
+		
+
+		return res.json({
+			todo: 'I will send you the shit'
+		})
+	}
+};
+
