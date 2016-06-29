499a42ae82aac084edf4480dbd692e5c5de2270d
Fredrik Broman <frebro@gmail.com>
2016-06-30 00:13:46 +0200

    Use herokuMysqlServer on development env

--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-  	connection: 'localMysqlServer',
+  	connection: 'herokuMysqlServer',
   	migrate: 'safe'
   }
 
